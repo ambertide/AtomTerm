88181fd4807c35ba84c23da652b818aeadceeaa4
Ege Özkan <egeemirozkan24@gmail.com>
2024-08-31 19:04:05 +0300

    fix: bash syntax

--- sysenable.sh
+++ sysenable.sh
@@ -2,7 +2,7 @@
 
 cd /home/ubuntu/AtomTerm
 
-if (! -f /etc/systemd/system/atomterm.service)
+if [ ! -f /etc/systemd/system/atomterm.service ]; then
     cp atomterm.service /etc/systemd/system/atomterm.service
     systemctl enable atomterm.service
     systemctl start atomterm.service
