4e0c2d7d12b652d463d6d81df0cab2c48f1a544f
Ege Özkan <egeemirozkan24@gmail.com>
2024-08-31 19:39:48 +0300

    feat: allow external access

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ $loader = require __DIR__ . '/vendor/autoload.php';
 
 $fs_parser = new \FSNavigation\NavigationParser(__DIR__ . '/menu');
 $menu = $fs_parser->parse();
-$socket = new NavigableSocket\NavigableSocket('127.0.0.1', 23, 5, $menu);
+$socket = new NavigableSocket\NavigableSocket('0.0.0.0', 23, 5, $menu);
 $socket->loop();
